e5ddf9a06674057f2cc692b6fa871985319e8b32
Mohd Labib <karibschool.web@gmail.com>
2023-01-17 17:47:07 +0700

    new

---
+++ main.py
@@ -0,0 +1,35 @@
+import re
+import pyautogui
+import serial.tools.list_ports
+
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+
+portsList = []
+
+for onePort in ports:
+    portsList.append(str(onePort))
+    print(str(onePort))
+
+val = input("Select Port: COM")
+
+for x in range(0,len(portsList)):
+    if portsList[x].startswith("COM" + str(val)):
+        portVar = "COM" + str(val)
+        print(portVar)
+
+serialInst.baudrate = 9600
+serialInst.port = portVar
+serialInst.open()
+
+while True:
+    data = serialInst.readline().decode().rstrip()
+    match = re.match("^[0-9]*$", data)
+    if match:
+        pyautogui.write(data)
+    
+
+    
+
+
+		
