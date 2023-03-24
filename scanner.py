f5eee3c5a636c65e2b552aeceea8915eabb62230
Mohd_Labib <59834085+mohdlabib@users.noreply.github.com>
2023-03-24 16:53:03 +0700

    update all

--- scanner.py
+++ scanner.py
@@ -2,59 +2,35 @@ import re
 import pyautogui
 import serial.tools.list_ports
 import serial
-import pystray
-from PIL import Image
 
-def get_serial_port():
-    ports = serial.tools.list_ports.comports()
-    portsList = []
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
 
-    for onePort in ports:
-        portsList.append(str(onePort))
-        print(str(onePort))
+portsList = []
 
-    while True:
-        val = input("Select Port: COM")
-        for x in range(0,len(portsList)):
-            if portsList[x].startswith("COM" + str(val)):
-                portVar = "COM" + str(val)
-                print(portVar)
-                return portVar
-        print("Port not found, please enter a valid port number.")
+for onePort in ports:
+    portsList.append(str(onePort))
+    print(str(onePort))
 
-def on_quit(icon, item):
-    icon.stop()
+val = input("Select Port: COM")
+
+for x in range(0,len(portsList)):
+    if portsList[x].startswith("COM" + str(val)):
+        portVar = "COM" + str(val)
+        print(portVar)
+
+serialInst.baudrate = 9600
+serialInst.port = portVar
+
+try:
     serialInst.close()
-    print("Application closed")
+except:
+    pass
 
-def main():
-    portVar = get_serial_port()
-    serialInst = serial.Serial(port=portVar, baudrate=9600)
+serialInst.open()
 
-    try:
-        serialInst.open()
-    except serial.SerialException as e:
-        print(f"Error opening serial port {portVar}: {e}")
-        return
-
-    icon = pystray.Icon('Scanner')
-    image = Image.open("icon.png")
-    icon.menu = pystray.Menu(
-        pystray.MenuItem(
-            'Quit', 
-            on_quit, 
-            default=True,
-        )
-    )
-    icon.icon = image
-    icon.title = 'Scanner'
-    icon.run(setup=icon.visible)
-
-    while True:
-        data = serialInst.readline().decode().rstrip()
-        match = re.match("^[0-9]*$", data)
-        if match:
-            pyautogui.write(data)
-
-if __name__ == '__main__':
-    main()
+while True:
+    data = serialInst.readline().decode().rstrip()
+    match = re.match("^[0-9]*$", data)
+    if match:
+        pyautogui.write(data)
